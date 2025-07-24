319f23246409aabbee1ff5dadf19eb6b0947bc72
wafaa-muhamed <w.abdulrazzaq@wakeb.tech>
2025-07-25 02:33:51 +0300

    Finished and tested example_nav_to_pose and example_nav_through_poses

--- nav2_cpp_simple_commander/examples/example_nav_through_poses.cpp
+++ nav2_cpp_simple_commander/examples/example_nav_through_poses.cpp
@@ -1,19 +1,104 @@
-// Requires implementations of the following functions:
+#include <chrono>
+#include <memory>
+#include <vector>
 
-// setInitialPose() – Set starting pose
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav2_cpp_simple_commander/navigator.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-// waitUntilNav2Active() – Wait for Nav2 to be ready
+using namespace std::chrono_literals;
+using namespace Nav2SimpleCommander;
 
-// goThroughPoses() – Navigate through multiple goals
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("go_through_poses_demo");
+  Navigator navigator(node);
 
-// isTaskComplete() – Check if done
+  // Set initial pose
+  geometry_msgs::msg::PoseWithCovarianceStamped initial_pose;
+  initial_pose.header.frame_id = "map";
+  initial_pose.header.stamp = node->now();
+  initial_pose.pose.pose.position.x = 3.45;
+  initial_pose.pose.pose.position.y = 2.15;
+  initial_pose.pose.pose.orientation.z = 1.0;
+  initial_pose.pose.pose.orientation.w = 0.0;
+  //   navigator.setInitialPose(initial_pose);
 
-// getFeedback() – Get ETA / progress
+  // Wait until navigation is active
+  //   navigator.waitUntilNav2Active();
 
-// cancelTask() – Cancel if too long
+  // Define goal poses
+  std::vector<geometry_msgs::msg::PoseStamped> goal_poses;
 
-// getResult() – Get final status
+  geometry_msgs::msg::PoseStamped pose;
+  pose.header.frame_id = "map";
 
-// lifecycleShutdown() – Shutdown Nav2
+  pose.pose.position.x = 0;
+  pose.pose.position.y = -1.5;
+  pose.header.stamp = node->now();
+  pose.pose.orientation.w = 0.707;
+  pose.pose.orientation.z = 0.707;
+  goal_poses.push_back(pose);
 
-// get_clock() – Timestamp for poses
+  pose.pose.position.x = -1;
+  pose.pose.position.y = -2;
+  pose.header.stamp = node->now();
+  goal_poses.push_back(pose);
+
+  pose.pose.position.x = -2;
+  pose.pose.position.y = 0;
+  pose.header.stamp = node->now();
+  goal_poses.push_back(pose);
+
+  // Send goal
+  navigator.goThroughPoses(goal_poses);
+
+  int i = 0;
+  while (!navigator.isTaskComplete()) {
+    auto feedback = navigator.getFeedback<Navigator::NavigateThroughPoses>();
+    if (feedback && i++ % 5 == 0) {
+      RCLCPP_INFO(
+        node->get_logger(), "ETA: %.0f seconds",
+        rclcpp::Duration(feedback->estimated_time_remaining).seconds());
+
+      if (rclcpp::Duration(feedback->navigation_time) > rclcpp::Duration(500.0s)) {
+        navigator.cancelTask();
+      }
+      //# Some navigation request change to demo preemption
+
+      if (rclcpp::Duration(feedback->navigation_time) > rclcpp::Duration(35.0s)) {
+        geometry_msgs::msg::PoseStamped new_goal;
+        new_goal.header.frame_id = "map";
+        new_goal.header.stamp = node->now();
+        new_goal.pose.position.x = -5.0;
+        new_goal.pose.position.y = -4.75;
+        new_goal.pose.orientation.w = 0.707;
+        new_goal.pose.orientation.z = 0.707;
+        navigator.goThroughPoses({new_goal});
+      }
+    }
+    rclcpp::sleep_for(500ms);
+  }
+
+  // Evaluate result
+  // Result handling
+  switch (navigator.getTaskResult()) {
+    case Navigator::TaskResult::SUCCEEDED:
+      RCLCPP_INFO(node->get_logger(), "Result :: Goal succeeded!");
+      break;
+    case Navigator::TaskResult::CANCELED:
+      RCLCPP_WARN(node->get_logger(), "Result ::Goal was canceled!");
+      break;
+    case Navigator::TaskResult::FAILED:
+      RCLCPP_ERROR(node->get_logger(), "Result ::Goal failed!");
+      break;
+    default:
+      RCLCPP_ERROR(node->get_logger(), "Result ::Goal returned unknown status.");
+      break;
+  }
+
+  //   navigator.lifecycleShutdown();
+  rclcpp::shutdown();
+  return 0;
+}
--- nav2_cpp_simple_commander/examples/example_nav_to_pose.cpp
+++ nav2_cpp_simple_commander/examples/example_nav_to_pose.cpp
@@ -1,26 +1,8 @@
-// Requires implementations of the following functions:
-// setInitialPose() – set robot’s start pose
-
-// waitUntilNav2Active() – wait for Nav2 to be ready
-
-// goToPose() – send goal pose
-
-// isTaskComplete() – check if navigation finished
-
-// getFeedback() – get progress updates
-
-// cancelTask() – cancel the goal if needed
-
-// getResult() – get final result status
-
-// lifecycleShutdown() – shutdown Nav2 nodes
-
-// get_clock() – get current ROS time
-
 #include <chrono>
 #include <memory>
 
 #include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
 #include "nav2_cpp_simple_commander/navigator.hpp"
 #include "rclcpp/rclcpp.hpp"
 
@@ -37,17 +19,19 @@ int main(int argc, char ** argv)
   geometry_msgs::msg::PoseWithCovarianceStamped initial_pose;
   initial_pose.header.frame_id = "map";
   initial_pose.header.stamp = node->now();
-  initial_pose.pose.pose.position.x = -2.0;
-  initial_pose.pose.pose.position.y = 0.0;
-  initial_pose.pose.pose.orientation.w = 1.0;
-
+  initial_pose.pose.pose.position.x = 3.45;
+  initial_pose.pose.pose.position.y = 2.15;
+  initial_pose.pose.pose.orientation.z = 1.0;
+  initial_pose.pose.pose.orientation.w = 0.0;
   // navigator.setInitialPose(initial_pose);
 
+  // Startup lifecycle (if not autostarted)
   // navigator.lifecycleStartup();
-  // // Wait for nav2 stack to become active
+
+  // Wait for Nav2 to become active
   // navigator.waitUntilNav2Active();
 
-  // // Define goal pose
+  // Define goal
   geometry_msgs::msg::PoseStamped goal_pose;
   goal_pose.header.frame_id = "map";
   goal_pose.header.stamp = node->now();
@@ -55,12 +39,60 @@ int main(int argc, char ** argv)
   goal_pose.pose.position.y = -0.5;
   goal_pose.pose.orientation.w = 1.0;
 
-  // // Send goal
-  navigator.goToPose(goal_pose);
+  // Send goal
+  if (!navigator.goToPose(goal_pose)) {
+    RCLCPP_ERROR(node->get_logger(), "Failed to send goal.");
+    rclcpp::shutdown();
+    return 1;
+  }
 
-  rclcpp::sleep_for(std::chrono::seconds(3));
-  navigator.cancelTask();
+  // Monitor task
+  int i = 0;
+  while (!navigator.isTaskComplete()) {
+    auto feedback = navigator.getFeedback<Navigator::NavigateToPose>();
+    if (feedback && i++ % 5 == 0) {
+      RCLCPP_INFO(
+        node->get_logger(), "ETA: %.0f seconds",
+        rclcpp::Duration(feedback->estimated_time_remaining).seconds());
 
+      // Cancel if taking too long
+      //Some navigation timeout to demo cancellation
+      if (rclcpp::Duration(feedback->navigation_time) > rclcpp::Duration(20.0s)) {
+        navigator.cancelTask();
+        break;
+      }
+
+      // Preempt after 18 seconds
+      // Some navigation request change to demo preemption
+      //it enters here very LATE
+      if (rclcpp::Duration(feedback->navigation_time) > rclcpp::Duration(18.0s)) {
+        RCLCPP_WARN(
+          node->get_logger(), "Preempting with new goal at nav_time = %.2f",
+          rclcpp::Duration(feedback->navigation_time).seconds());
+        goal_pose.pose.position.y = 0.5;
+        navigator.goToPose(goal_pose);
+      }
+    }
+    rclcpp::sleep_for(500ms);
+  }
+
+  // Result handling
+  switch (navigator.getTaskResult()) {
+    case Navigator::TaskResult::SUCCEEDED:
+      RCLCPP_INFO(node->get_logger(), "Result :: Goal succeeded!");
+      break;
+    case Navigator::TaskResult::CANCELED:
+      RCLCPP_WARN(node->get_logger(), "Result ::Goal was canceled!");
+      break;
+    case Navigator::TaskResult::FAILED:
+      RCLCPP_ERROR(node->get_logger(), "Result ::Goal failed!");
+      break;
+    default:
+      RCLCPP_ERROR(node->get_logger(), "Result ::Goal returned unknown status.");
+      break;
+  }
+
+  // Shutdown Nav2
   // navigator.lifecycleShutdown();
 
   rclcpp::shutdown();
